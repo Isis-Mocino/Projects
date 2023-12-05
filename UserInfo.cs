eff7c176f178fffbef7cd0aaeefd0b6b97b4633f
Isis Mociño <83838132+Isis-Mocino@users.noreply.github.com>
2023-12-04 21:37:19 -0600

    Delete UserInfo.cs

--- UserInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nutricion.API.Models
-{
-    public class UserInfo
-    {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
